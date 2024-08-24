2ce9bc0d0c223ee64d6c416645a792c7d0e9afac
Sprum <108943595+Sprum@users.noreply.github.com>
2024-08-24 23:13:16 +0200

    refactored err logging to not break it for now

--- client/client.go
+++ client/client.go
@@ -23,7 +23,7 @@ func (c *Client) SendFile() {
 	url := fmt.Sprintf("%s:8080", ipAddress)
 	file, err := os.Open(filePath)
 	if err != nil {
-		log.Fatalf("Error opening file: %v", err)
+		fmt.Println("Error opening file: %v", err)
 	}
 	defer file.Close()
 	// Extract filename
@@ -32,7 +32,7 @@ func (c *Client) SendFile() {
 	// establish connection
 	con, err := net.Dial("tcp", url)
 	if err != nil {
-		log.Fatalf("Error connecting to server: %v", err)
+		fmt.Println("Error connecting to server: %v", err)
 	}
 	defer con.Close()
 	fmt.Println("connection established, waiting for handshake.")
@@ -53,14 +53,14 @@ func (c *Client) SendFile() {
 	copy(filenameBuf, filename)
 	_, err = con.Write(filenameBuf)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err)
 	}
 	fmt.Println("Filename sent. Sending file data...")
 
 	// 3. send file data
 	_, err = io.Copy(con, file)
 	if err != nil {
-		log.Fatalf("Error sending file data: %v", err)
+		fmt.Println("Error sending file data: %v", err)
 	}
 	fmt.Println("File sent successfully.")
 	return
